8379dbe27f4c089611cd045af6c78c4c97ac2d03
Himanshu Kumar <himanshu95.2010@gmail.com>
2020-06-05 01:22:57 +0530

    manipulating the state

--- src/App.js
+++ src/App.js
@@ -3,11 +3,33 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+
+  state = {
+    persons: [
+      {name: 'Max'},
+      {name: 'Lily'},
+      {name: 'Jack'},
+    ]
+  }
+
+  switchNameHandler = () => {
+    this.setState({
+      persons: [
+        {name: 'Maximiliam'},
+        {name: 'Lily'},
+        {name: 'Jack'},
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hello World!!!</h1>
-        <Person/>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} />
+        <Person name={this.state.persons[1].name}>Alan Walker</Person>
+        <Person name={this.state.persons[2].name} />
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React from "react";
 
-const person = () => {
-  return <p>I'm a Person!</p>
+// Function based component creation
+const person = (props) => {
+  return (
+    <div>
+      <p>I'm {props.name}</p>
+      <p>{props.children}</p>
+    </div>
+  )
+};
+
+
+// Class based component creation
+/*
+class person extends Component {
+  render() {
+    return <p>I'm {this.props.name} from class based component.</p>;
+  }
 }
+*/
 
 export default person;
