6a6cbe1aa1b9c65e75272c84ccf2f773fc2da32c
Himanshu Kumar <himanshu95.2010@gmail.com>
2020-09-08 12:29:34 +0530

    Adding and using Radium for css pseudo classes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Radium from 'radium';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -44,7 +45,11 @@ class App extends Component {
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     }
 
     let persons = null;
@@ -63,6 +68,10 @@ class App extends Component {
       );
 
       buttonStyle.backgroundColor = 'red';
+      buttonStyle[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black'
+      }
     }
 
     const classes = [];
@@ -84,4 +93,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Radium(App);
